2ad46dcfcf681862df7a92020a0816f31cbfa64f
Luiz Lelis <luizhlelis@gmail.com>
2022-11-21 12:45:51 -0300

    fix: existing tests with `create_superuser` 🧪

--- tests/integration/test_user_view.py
+++ tests/integration/test_user_view.py
@@ -1,28 +1,32 @@
 from datetime import date
+from django.contrib.auth.models import User
 
 import pytest
+import base64
 
+@pytest.mark.django_db
 class TestUserAPIView:
     def test_get_users_should_return_200(self, client):
         # Arrange
-        fake_data = {}
-        header = {
-            "HTTP_AUTHORIZATION": "Bearer fake-token",
-        }
+        self.user = User.objects.create_superuser(
+            username='foobar',
+            email='foo@bar.com',
+            password='barbaz')
+        client.force_login(user=self.user)
         # Act
-        response = client.get("/users", content_type="application/json", data=fake_data, auth=("admin", "admin123"))
+        response = client.get("/users", follow=True)
         # Assert
         assert response.status_code == 200
         result = response.json()
         assert result == {
             "count": 1,
-            "next": "null",
-            "previous": "null",
+            "next": None,
+            "previous": None,
             "results": [
                 {
-                    "url": "http://127.0.0.1:8000/users/1/",
-                    "username": "admin",
-                    "email": "admin@example.com",
+                    "url": "http://testserver/users/1/",
+                    "username": "foobar",
+                    "email": "foo@bar.com",
                     "groups": []
                 }
             ]
